36a3d4ad85a863b8f6702b90bb0c94b42a37dcce
Perpetual Meninwa <perpetualmeninwa@gmail.com>
2024-05-09 13:13:34 +0100

    chore: dynamically render the featured and not featured articles and the categories from the database

--- routes/index.js
+++ routes/index.js
@@ -4,20 +4,34 @@ var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  let navigationLinks = [
-      'Multiplex PCR', 'Technology', 'Design', 'Culture', 'Business', 'Sports'
-  ];
+  // let navigationLinks = [
+  //     'Multiplex PCR', 'Technology', 'Design', 'Culture', 'Business', 'Sports'
+  // ];
 
   let rawdata = fs.readFileSync('./database/posts.json');
 
   let posts = JSON.parse(rawdata);
 
-  let featuredPosts = posts.filter('is_featured')
+  // return only the categories
+  let categories = posts.map(post => post.category);
+
+  // return only the unique categories
+  const uniqueCategories = new Set(categories);
+
+  // convert the Set back to array
+  const itemCategories = Array.from(uniqueCategories);
+
+  // Return an array of items with the is_featured set to true
+  const featuredArticles = posts.filter(post => post.is_featured);
+
+  // Return an array of items with the is+featured set to false
+  const notFeaturedArticles = posts.filter(post => !post.is_featured);
 
   res.render('blog', {
     title: 'She Code Queens',
-    links: navigationLinks,
-    posts: posts
+    links: itemCategories,
+    featuredPosts: featuredArticles,
+    posts: notFeaturedArticles
   });
 
 });
